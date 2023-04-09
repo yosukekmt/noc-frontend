fa896bff8c75a5312dbb4c9ff1dee8d8edaec18d
Yosuke Kimoto <yosuke.kmt@gmail.com>
2023-04-10 01:56:51 +0800

    Update UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,6 @@
 import Logo from "@/components/logo";
 import { useFirebase } from "@/hooks/useFirebase";
 import {
-  AtSignIcon,
-  ChatIcon,
-  ChevronRightIcon,
-  MoonIcon,
-  QuestionIcon,
-} from "@chakra-ui/icons";
-import {
   Box,
   Button,
   Container,
@@ -16,11 +9,19 @@ import {
   GridItem,
   Heading,
   HStack,
+  Icon,
   Spacer,
   Text,
 } from "@chakra-ui/react";
 import Head from "next/head";
 import NextLink from "next/link";
+import {
+  ArrowRight,
+  ChartLineUp,
+  Cursor,
+  TestTube,
+  Ticket,
+} from "phosphor-react";
 
 const Header = () => {
   const { authToken } = useFirebase();
@@ -37,7 +38,7 @@ const Header = () => {
             <NextLink href="/dashboard">
               <Button
                 size="sm"
-                rightIcon={<ChevronRightIcon />}
+                rightIcon={<Icon as={ArrowRight} />}
                 style={{ borderRadius: 20 }}
               >
                 Dashboard
@@ -71,14 +72,18 @@ const HeroSection = () => {
           <GridItem colSpan={{ base: 12, md: 12 }}>
             <Box my={{ base: 16, md: 36 }}>
               <Heading as="h1" size="3xl" lineHeight={1.4}>
-                Design Your Nudge For
+                LTV and CAC
                 <br />
-                Incentive Economy
+                management platform
                 <br />
-                Era
+                for the blockchain
               </Heading>
               <Heading as="h3" size="md" fontWeight="normal" mt={4}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Every blockchain business deserves a sophisticated LTV and CAC
+                management tool. Nudge ONCHAIN serves as a valuable companion in
+                designing the Nudge for the forthcoming incentive economy era,
+                enabling businesses to optimize their operations and thrive in
+                the rapidly-evolving blockchain landscape.
               </Heading>
             </Box>
           </GridItem>
@@ -95,18 +100,27 @@ const WhatIsSection = () => {
         <Grid templateColumns="repeat(12, 1fr)" gap={4} py={24}>
           <GridItem colSpan={{ base: 12, md: 6 }}>
             <Heading as="h1" size="xl">
-              What is &quot;NUDGE ONCHAIN&quot;?
+              What is &quot;Nudge ONCHAIN&quot;?
             </Heading>
           </GridItem>
           <GridItem colSpan={{ base: 12, md: 6 }}>
             <Text fontSize="lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum. 　
+              Nudge ONCHAIN offers a streamlined solution for managing cashback
+              systems on blockchain-based transactions. By utilizing the open
+              and accessible nature of the blockchain, we can analyze purchase
+              history data to determine optimal selling prices. This approach
+              enables businesses to effectively monitor and control Customer
+              Lifetime Value (LTV) and Customer Acquisition Cost (CAC), ensuring
+              a more efficient and competitive operation.
+            </Text>
+            <Heading as="h4" size="md" mt={8}>
+              Why Cashback?
+            </Heading>
+            <Text fontSize="lg">
+              Utilizing cashback systems for promoting purchasing decisions can
+              influence consumer decision-making without constraining their
+              choices. This approach allows for a precise measurement of LTV and
+              CAC.
             </Text>
           </GridItem>
         </Grid>
@@ -125,57 +139,62 @@ const FeaturesSection = () => {
             </Heading>
           </GridItem>
           <GridItem colSpan={{ base: 12 }}>
-            <Grid templateColumns="repeat(12, 1fr)" gap={2}>
+            <Grid templateColumns="repeat(12, 1fr)" gap={4}>
               <GridItem colSpan={{ base: 12, md: 6, lg: 3 }}>
                 <Box py={8}>
-                  <ChatIcon boxSize={6} />
+                  <Ticket size={32} />
                   <Heading as="h4" size="md" fontWeight="bold" py={4}>
-                    Feature 1
+                    Auto-cashback NFT
                   </Heading>
                   <Text fontSize="lg">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
+                    You can make an auto-cashback NFT for your customers. When
+                    they have this NFT, they&apos;ll get cashback for any
+                    transactions they make with your NFT-related purchases. You
+                    can even set how long the cashback campaign lasts and which
+                    NFTs are part of the deal.
                   </Text>
                 </Box>
               </GridItem>
               <GridItem colSpan={{ base: 12, md: 6, lg: 3 }}>
                 <Box py={8}>
-                  <QuestionIcon boxSize={6} />
+                  <Cursor size={32} />
                   <Heading as="h4" size="md" fontWeight="bold" py={4}>
-                    Feature 2
+                    Minting
                   </Heading>
                   <Text fontSize="lg">
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                    irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu fugiat nulla pariatur.
+                    You will then get this easy no-code minting page for you to
+                    share with your customers. They can mint their cashback NFTs
+                    for free, and they don&apos;t need to be tech-savvy to do
+                    it.
                   </Text>
                 </Box>
               </GridItem>
               <GridItem colSpan={{ base: 12, md: 6, lg: 3 }}>
                 <Box py={8}>
-                  <MoonIcon boxSize={6} />
+                  <ChartLineUp size={32} />
                   <Heading as="h4" size="md" fontWeight="bold" py={4}>
-                    Feature 3
+                    Realtime Dashboard
                   </Heading>
                   <Text fontSize="lg">
-                    Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
+                    Our dashboard provides a convenient way to manage your
+                    cashback rewards and NFT holders. With real-time updates,
+                    you can easily keep track of your customers&apos; behavior,
+                    enabling you to make informed decisions for your business.
                   </Text>
                 </Box>
               </GridItem>
               <GridItem colSpan={{ base: 12, md: 6, lg: 3 }}>
                 <Box py={8}>
-                  <AtSignIcon boxSize={6} />
+                  <TestTube size={32} />
                   <Heading as="h4" size="md" fontWeight="bold" py={4}>
-                    Feature 4
+                    Testnet
                   </Heading>
                   <Text fontSize="lg">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptatem accusantium doloremque laudantium, totam rem
-                    aperiam, eaque ipsa quae ab illo inventore veritatis et
-                    quasi architecto beatae vitae dicta sunt explicabo.
+                    If you&apos;re curious about how our system works, you can
+                    try the testnet version of Nudge ONCHAIN. You can experiment
+                    as much as you&apos;d like without any risks, so it&apos;s a
+                    great way to get comfortable with the platform before using
+                    it for real.
                   </Text>
                 </Box>
               </GridItem>
